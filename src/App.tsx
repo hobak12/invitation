0a09f427bd293ae6ed8c8418238d9c4da3967eb2
hobak12 <tjsgud2564@naver.com>
2023-05-11 03:00:43 +0300

    feat: firebase timestamp 변환

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,10 @@ import {
   useUpdateCommentMutation,
 } from "./redux/modules/apiSlice";
 import { debounce } from "lodash";
+import { Timestamp } from "@firebase/firestore";
 
 export interface CommentType {
-  createdAt?: number;
+  createdAt?: any;
   id?: string;
   name: string;
   context: string;
@@ -24,8 +25,12 @@ const App = () => {
   const trimName = name && name.trim();
   const trimContext = context && context.trim();
 
+  {
+    data?.map((item) => console.log(String(item.createdAt.toDate())));
+  }
+
   const newComment = {
-    createdAt: Date.now(),
+    createdAt: new Date(),
     name: trimName,
     context: trimContext,
   };
@@ -60,42 +65,49 @@ const App = () => {
   }
 
   return (
-    <div>
-      {data?.map((item) => {
-        return (
-          <div key={item.id}>
-            <div>{item.name}</div>
-            <div>{item.context}</div>
-          </div>
-        );
-      })}
-      <div>
-        <div>music</div>
-        <div> 왜 안뜨지?</div>
-        <div>김선형 & 판데이 수단슈</div>
-        <div>결혼합니다</div>
-        <div>2023년 9월 15일 토요일 오전 10시 30분</div>
-        <div>세종대왕 기념관 궁중의례원</div>
-        <button>참석여부 조사</button>
-        <div>갤러리</div>
-        <div>방명록</div>
-        <div> 마음전하실 곳</div>
-        <div> 달력</div>
-        <div> 지도 </div>
-        <div> 공유</div>
+    <div className="flex justify-center bg-pink-200 ">
+      <div className="bg-purple-100">
+        {data?.map((item) => {
+          return (
+            <div key={item.id}>
+              <div>{item.context}</div>
+              <div>{item.name}</div>
+              <div>{String(item.createdAt.toDate()).slice(0, 21)}</div>
+              <button>수정</button>
+              <button>삭제</button>
+              <div>비밀번호</div>
+              <div>{}</div>
+            </div>
+          );
+        })}
+        <div>
+          <div>music</div>
+          <div>영어/한국어</div>
+          <div>김선형 & 판데이 수단슈</div>
+          <div>결혼합니다</div>
+          <div>2023년 9월 15일 토요일 오전 10시 30분</div>
+          <div>세종대왕 기념관 궁중의례원</div>
+          <button>참석여부 조사 모달</button>
+          <div>갤러리</div>
+          <div>방명록</div>
+          <div> 마음전하실 곳</div>
+          <div> 달력</div>
+          <div> 지도 </div>
+          <div> 공유</div>
 
-        <form
-          onSubmit={(e) => onSubmitCommentHandler(e, addComment, newComment)}
-        >
-          <label>성명</label>
-          <input ref={nameRef} onChange={(e) => onChangeNameHandler(e)} />
-          <label>코멘트</label>
-          <textarea
-            ref={contextRef}
-            onChange={(e) => onChangeContextHandler(e)}
-          />
-          <button>입력</button>
-        </form>
+          <form
+            onSubmit={(e) => onSubmitCommentHandler(e, addComment, newComment)}
+          >
+            <label>성명</label>
+            <input ref={nameRef} onChange={(e) => onChangeNameHandler(e)} />
+            <label>코멘트</label>
+            <textarea
+              ref={contextRef}
+              onChange={(e) => onChangeContextHandler(e)}
+            />
+            <button>입력</button>
+          </form>
+        </div>
       </div>
     </div>
   );
