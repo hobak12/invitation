648ce902a01e1267b7f012c5aff792aa84d4e2a5
hobak12 <tjsgud2564@naver.com>
2023-06-05 17:55:45 +0300

    feat: scroll up reducing gallery

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import GalleryModal from "./GalleryModal";
 import SliderData from "./SliderData";
+import { BsChevronDown, BsChevronUp } from "react-icons/bs";
 const Gallery = () => {
   const [open, setOpen] = useState<boolean>(false);
+  const element = useRef<HTMLDivElement>(null);
+
+  const onMoveToElement = () => {
+    element.current?.scrollIntoView({
+      block: "start",
+    });
+  };
+
   const onClickToggleGallery = () => {
     setOpen((current) => !current);
+  };
+
+  const onClickClose = () => {
+    onMoveToElement();
+    onClickToggleGallery();
   };
   return (
     <div>
       <div className={`${open ? "" : "h-[500px]"}  overflow-hidden`}>
-        <div className="flex flex-wrap">
+        <div ref={element} className="flex flex-wrap">
           {SliderData.map((slide, index) => {
             return (
               <div className="w-[50%] " key={index}>
@@ -23,11 +37,14 @@ const Gallery = () => {
           })}
         </div>
       </div>
-      <button onClick={onClickToggleGallery} className="button">
-        더보기
+      <button
+        onClick={onClickToggleGallery}
+        className={`${open ? "hidden" : ""}`}
+      >
+        <BsChevronDown />
       </button>
-      <button onClick={onClickToggleGallery} className="button">
-        작게보기
+      <button onClick={onClickClose} className={`${open ? "" : "hidden"} `}>
+        <BsChevronUp />
       </button>
       <GalleryModal SliderData={SliderData} />
     </div>
