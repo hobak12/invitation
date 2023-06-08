eeedeb508f8d8af406832f09746ae84a570713f4
hobak12 <tjsgud2564@naver.com>
2023-06-08 07:10:49 +0300

    design: inviting sentence done

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -8,34 +8,47 @@ import { Gallery } from "../components/Gallery";
 const Main = () => {
   const { t } = useTranslation("main");
   return (
-    <div className="bg-orange-100 bg-opacity-20 font-basic text-lg mx-auto w-[650px] h-[100%] md:w-full shadow-xl ')]">
+    <div className="bg-orange-100 bg-opacity-20 font-basic text-lg mx-auto w-[600px] h-[100%] sm:w-full shadow-xl ')]">
       <div className="flex flex-col items-center">
         <div className="flex items-center mt-2  w-full justify-between">
           <Language />
           <Music />
         </div>
-        <div className="flex items-baseline mt-[8%] mb-[4%] ml-8">
-          <div className="text-xl mr-2">{t("brideName")} </div>
-          <div className="text-xs mr-2 ">{t("and")}</div>
-          <div className="text-xl"> {t("groomName")}</div>
+        <div className="my-[10%] text-center">
+          <div className="flex items-baseline mb-[4%] ml-8">
+            <div className="text-xl mr-2">{t("brideName")} </div>
+            <div className="text-xs mr-2 ">{t("and")}</div>
+            <div className="text-xl"> {t("groomName")}</div>
+          </div>
+          <div className="text-2xl ">{t("married")}</div>
         </div>
-        <div className="text-2xl">{t("married")}</div>
         <img
-          className="w-[90%] my-[7%]"
+          className="w-[90%] mb-[5%]"
           alt="marriage"
           src="/assets/marriage.png"
         />
-        <div>{t("time")}</div>
-        <div className="mb-[10%]">{t("place")}</div>
-        <div className="text-center ">
-          인연으로 만나 부부가 되는 날,
-          <br />
-          소중한 분들을 초대합니다.
-          <br />
-          결혼식은 한국 전통혼례로 야외에서 진행할 예정입니다.
+        <div className="mb-[5%] border-2  p-4 text-center bg-white rounded-lg">
+          <div className="mb-2">{t("time")}</div>
+          <div>{t("place")}</div>
         </div>
+
+        <div className="mb-[20%]">
+          결혼식은 <span className="font-semibold text-lg">한국 전통혼례</span>
+          로 <span className="font-semibold text-lg">야외</span>에서 진행합니다.
+        </div>
+        <div className="text-center leading-loose">
+          평생 함께하고 싶은 사람을 만나
+          <br />
+          부부의 연을 맺고자 합니다.
+          <br />
+          서로 아끼고 사랑하며 살겠습니다.
+          <br />
+          참석해주셔서 축복해주시면
+          <br />큰 기쁨으로 간직하겠습니다.
+        </div>
+
         <button
-          className="button my-[10%]"
+          className="bg-[#ffdadf] my-[15%] rounded py-1 px-2 font-semibold text-xl shadow-sm hover:shadow-md"
           onClick={() => {
             window.open(
               "https://docs.google.com/forms/d/e/1FAIpQLSfWSB34WkeIedPglfTlANpQWqWBSYzUesLuv5c6J9CBWiwlqA/viewform?usp=sf_link"
@@ -44,22 +57,31 @@ const Main = () => {
         >
           {t("attendance")}
         </button>
-        <div className="flex gap-11">
-          <div className="text-center m-5 ">
-            {t("groomFatherName")}, {t("groomMotherName")}
-            {t("'s")} {t("son")}
-            <div className="text-xl my-2"> {t("groomName")}</div>
+        <div className="sm:flex-col flex gap-12 mb-[20%] text-center">
+          <div className="">
+            <div>
+              {t("groomFatherName")} · {t("groomMotherName")}
+            </div>
+            <div className="text-sm mx-1 mt-2 mb-1 ">
+              {t("'s")} {t("son")}
+            </div>
+            <div className="my-2 "> {t("groomName")}</div>
           </div>
-          <div className="text-center m-5 ">
-            {t("brideFatherName")}, {t("brideMotherName")}
-            {t("'s")} {t("daughter")}
-            <div className="text-xl my-2">{t("brideName")}</div>
+
+          <div className="">
+            <div>
+              {t("brideFatherName")} · {t("brideMotherName")}
+            </div>
+            <div className="text-sm mx-1 mt-2 mb-1 ">
+              {t("'s")} {t("daughter")}
+            </div>
+            <div className=" my-2">{t("brideName")}</div>
           </div>
         </div>
         <Gallery />
         <Calendar t={t} />
 
-        <div className="">{t("comment")}</div>
+        <div className=" title">{t("comment")}</div>
 
         <Comment />
       </div>
