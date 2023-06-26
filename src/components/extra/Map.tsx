fcb546171e1e124160683dae2c101075555dd7c9
hobak12 <tjsgud2564@naver.com>
2023-06-26 17:50:31 +0300

    feat: map reset button

--- src/components/extra/Map.tsx
+++ src/components/extra/Map.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { FaBus, FaSubway, FaInfo, FaMapMarkerAlt } from "react-icons/fa";
 import { ImPhone } from "react-icons/im";
+import { GrPowerReset } from "react-icons/gr";
 
 declare global {
   interface Window {
@@ -47,6 +48,48 @@ const Map = ({ t }: any) => {
     infowindow.open(map, marker);
   }, []);
 
+  const setCenter = () => {
+    const container = document.getElementById("map");
+    const options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(37.59035984926336, 127.04067085111706), //지도의 중심좌표.
+      level: 4, //지도의 레벨(확대, 축소 정도)
+    };
+    const map = new kakao.maps.Map(container, options);
+    var moveLatLon = new kakao.maps.LatLng(
+      37.59035984926336,
+      127.04067085111706
+    );
+    // 지도 중심을 이동 시킵니다
+    map.setCenter(moveLatLon);
+
+    // 마커가 표시될 위치입니다
+    const markerPosition = new kakao.maps.LatLng(
+      37.59073497281094,
+      127.04358664439376
+    );
+
+    // 마커를 생성합니다
+    const marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+
+    var iwContent =
+        '<div style="font-size:14px; padding-left:4px;">세종대왕기념관 웨딩홀</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+      iwPosition = new kakao.maps.LatLng(37.59073497281094, 127.04358664439376); //인포윈도우 표시 위치입니다
+
+    // 인포윈도우를 생성하고 지도에 표시합니다
+    var infowindow = new kakao.maps.InfoWindow({
+      map: map, // 인포윈도우가 표시될 지도
+      position: iwPosition,
+      content: iwContent,
+    });
+    // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+    infowindow.open(map, marker);
+  };
+
   return (
     <div className="mb-[20%] ">
       <div className=" title  ">{t("map")}</div>
@@ -55,7 +98,9 @@ const Map = ({ t }: any) => {
         <div className="mb-2">{t("time")}</div>
         <div>{t("place")}</div>
       </div>
-
+      <button onClick={setCenter}>
+        <GrPowerReset className="relative top-10 left-2 z-[55] w-[30px] h-[30px] rounded p-1 bg-white" />
+      </button>
       <div id="map" className="w-full h-[400px] sm:h-[300px]"></div>
       <div className="flex justify-center gap-10 my-[5%] ">
         <button
--- src/components/keyframe/Petals.tsx
+++ src/components/keyframe/Petals.tsx
@@ -1,8 +1,21 @@
 const Petals = ({ id, style }: any) => {
   return (
-    <p className="petals" id={`item${id}`} style={style}>
-      *
-    </p>
+    <>
+      <p
+        className="petals inline-block sm:hidden"
+        id={`item${id}`}
+        style={style}
+      >
+        *
+      </p>
+      <p
+        className="petals1 hidden sm:inline-block "
+        id={`item${id}`}
+        style={style}
+      >
+        *
+      </p>
+    </>
   );
 };
 
